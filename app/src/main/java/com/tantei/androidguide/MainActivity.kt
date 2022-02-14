6f962e1a438045898b3685340a5cdde0b6b4ce68
tantei <tanteichang@outlook.com>
2022-02-14 14:17:34 +0800

    feat(app): 完成第三章

--- app/src/main/java/com/tantei/androidguide/MainActivity.kt
+++ app/src/main/java/com/tantei/androidguide/MainActivity.kt
@@ -2,11 +2,14 @@ package com.tantei.androidguide
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.Gravity
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+
+private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
 
@@ -27,6 +30,8 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d(TAG, "onCreate: called")
+        
         setContentView(R.layout.activity_main)
 
         trueButton = findViewById(R.id.ture_button)
@@ -63,5 +68,30 @@ class MainActivity : AppCompatActivity() {
         }
 
         Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart: called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume: called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause: called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop: called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy: called")
     }
 }
